22d491e1f57ca58a46e31c7649429b6343583d1c
Donovan McRae <d_mcrae@ymail.com>
2016-05-16 00:16:26 -0400

    adding form in user#new; displaying users is user#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+
+
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if params[:name] != ""
+      @user.save
+
+      redirect_to users_path
+    else
+      render "new"
+    end
   end
 
   def index
     @users = User.all
-    
+
   end
 
   def edit
@@ -18,4 +30,9 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :age, :email, :bio, :avatar)
+    end
 end
